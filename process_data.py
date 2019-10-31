8722c5f5ce1b4a372d3b5b8502c058d8b18b123a
Luke Fox <l.fox9@nuigalway.ie>
2019-10-31 21:05:51 +0000

    added method to normalise features

--- process_data.py
+++ process_data.py
@@ -1,6 +1,5 @@
 import pandas as pd 
 
-
 class svm():
 
     def getClasses(self, data):
@@ -19,13 +18,24 @@ class svm():
    
     def output(self,class_dictionary):
         for i in range(self.numClasses):
-            location = str('/home/luke/ml_ct4101/assignment2/data_split/' + class_dictionary[i] + '_data.csv')
+            location = str('/home/luke/ml_ct4101/assignment2/data_split/' + class_dictionary[i] + '.csv')
             name = "class_"+ str(i) +"_data"
             getattr(self,name).to_csv(location, index=None)
+
+    def normalise(self, data):
+            copy = data.copy()
+            for feature_name in data.columns:
+                if feature_name != "class":
+                    copy[feature_name] = (data[feature_name] - data[feature_name].mean()) / data[feature_name].std()
+                else:
+                    copy[feature_name] = data[feature_name]
+            return copy 
+
 
 if __name__ == '__main__':
     s = svm()
     data = pd.read_csv("hazelnuts.csv")
+    data = s.normalise(data)
     class_dictionary = s.getClasses(data)
     s.processData(data,class_dictionary)
     s.output(class_dictionary)
